edf257774b69756459f154ec899fc508ced81770
AnjiYukiu <110330448+AnjiYukiu@users.noreply.github.com>
2022-07-31 11:45:50 +0200

    Add files via upload
    Curso de introducción a la programación:
       Ejercicio 1: Creación de una función que suma tres parámetros dados y devuelve el resultado

---
+++ Ejercicio1Suma3Parametros.java
@@ -0,0 +1,11 @@
+public class Ejercicio1Suma3Parametros {
+    public static void main(String[] args) {
+        var result = suma(10, 30,32);
+        System.out.println("El resultado de la suma del ejercicio es "+ result);
+    }
+    public static int suma(int a, int b, int c) {
+
+        return a + b + c;
+    }
+
+}
